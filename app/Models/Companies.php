90eb644b569ee0f1d8f830f36a7149d07f8036f7
alldigital <darcio.soares@agenciaalldigital.com>
2022-01-25 12:45:42 -0300

    criação da migration e model de companies e profile. Tambem foi criado o relacionamento entre users, profiles e companies e configudado os models

---
+++ app/Models/Companies.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Companies extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'cnpj',
+        'social_Reason',
+        'fantasy_name',
+        'status',
+        'name_responsible',
+        'last_name_responsible',
+        'email_responsible',
+        'color',
+        'logo',
+        'monthly_payment',
+        'activation',
+        'price_per_plate',
+        'user_limit',
+        'price_per_extra_user',
+        'user_id'
+    ];
+
+
+    public function service()
+    {
+        return $this->hasOne(Services::class);
+    }
+
+    public function equipment()
+    {
+        return $this->hasOne(Equipaments::class);
+    }
+
+}//end class 
---
+++ database/migrations/2022_01_24_233521_create_companies_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('cnpj');
+            $table->string('social_Reason');
+            $table->string('fantasy_name');
+            $table->string('status');
+            $table->string('name_responsible');
+            $table->string('last_name_responsible');
+            $table->string('email_responsible');
+            $table->string('color');
+            $table->string('logo');
+            $table->string('monthly_payment');
+            $table->string('activation');
+            $table->string('price_per_plate');
+            $table->string('user_limit');
+            $table->string('price_per_extra_user');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
