af718d381a8933fae3b0b62e4348cbfda7563af4
alldigital <darcio.soares@agenciaalldigital.com>
2022-02-21 16:21:44 -0300

    criação de um template teste

---
+++ resources/views/teste2.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app2')
+
+@section('nav-header')
+
+<!-- start page title -->
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-flex align-items-center justify-content-between">
+            <h4 class="page-title mb-0 font-size-18">Cliente</h4>
+        </div>
+    </div>
+</div>
+<!-- end page title -->
+
+@endsection
+
+@section('content')
+
+    <h1>Teste 2 </h1>
+@endsection
+
+@push('script-js')
+    <script src="{{asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+    
+    <script src="{{asset('assets/libs/jquery-steps/build/jquery.steps.min.js')}}"></script>
+
+   
+    <script src="{{asset('assets/js/app.js')}}"></script>
+
+    <script src="{{asset('assets\libs\inputmask\min\jquery.inputmask.bundle.min.js')}}"></script>
+    
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+@endpush
+
