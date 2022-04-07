6bcacc24d2f0477a16d9573fd3787a8564badb74
alldigital <darcio.soares@agenciaalldigital.com>
2022-04-07 19:43:06 -0300

    criação do form request para validar os dados

---
+++ app/Http/Requests/UserCreateRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'create_user_company' => 'required|min:6',
+            'create_user_status' => 'required',
+            'create_user_loginEmail' => 'required|min:6',
+            'create_user_perfil' => 'required|min:6',
+            'create_user_upper' => 'required|min:6',
+            'user_create_name' => 'required|min:6',
+            'user_create_name' => 'required|min:6',
+            'user_create_lastname' => 'required|min:6',
+            // 'create_user_avatar' => 
+            'create_user_date_insert' => 'required|date',
+            'create_user_user_insert' => 'required|date',
+
+
+            'create_user_password' => 'required|min:6',
+            'create_user_password_confirm' => 'required_with:create_user_password|same:create_user_password|min:6'                     
+        ];
+    }
+
+    //  valid -> date 'required|date_format:d/m/Y';
+}
