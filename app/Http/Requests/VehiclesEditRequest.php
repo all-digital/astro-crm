38471df3caa3e79721d500a93e0e8b79ba927a2d
alldigital <darcio.soares@agenciaalldigital.com>
2022-04-08 17:27:03 -0300

    criando e add as regras no form request tanto de criação como edição de veiculos

---
+++ app/Http/Requests/VehiclesEditRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VehiclesEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'company' => 'required|min:2|max:155',
+            'status' => 'required|max:20',
+            'vehicles' => 'required|min:2|max:155',
+            'brand' => 'required|min:2|max:155',
+            'model' => 'required|min:2|max:155',
+            'year' => 'required',
+            'vehicle_plate' => 'required',
+            'value' => 'required',
+        ];
+    }
+}
