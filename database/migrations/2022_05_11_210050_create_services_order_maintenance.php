4405d86e2db902fc09be21dbb73a6519fe0bcfe1
alldigital <darcio.soares@agenciaalldigital.com>
2022-05-11 18:08:54 -0300

    criando as migrations da tabela cliente (customers) e orders de serviços

---
+++ database/migrations/2022_05_11_210050_create_services_order_maintenance.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesOrderMaintenance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services_order_maintenance', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer');
+            $table->string('status');
+            $table->string('company');
+            $table->string('type_order_of_service');
+            $table->string('vehicles');
+            $table->string('equipments');
+            $table->string('simcard');
+            $table->string('services');
+            $table->string('adhesion_fee');
+            $table->string('monthly_payment');
+            $table->string('recurrence');
+            $table->string('customer_adress');
+            $table->string('responsible_for_insert');
+            $table->string('services_order_sale');
+            $table->string('subtype');
+            $table->string('adress_customer');
+            $table->string('zip_code_customer');
+            $table->string('city_customer');
+            $table->string('country_customer');
+
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services_order_maintenance');
+    }
+}
